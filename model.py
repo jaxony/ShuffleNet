b9d75928f3e6fbdc979240007faf469283107855
Jackson Huang <jackson_huang@live.com>
2017-07-15 20:36:57 +1000

    added class skeletons

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class ShuffleUnit(nn.Module):
+    def __init__():
+        pass
+
+    def forward(x):
+        pass
+
+
+class ShuffleNet(nn.Module):
+
+    def __init__():
+        pass
+
+    def forward(x):
+        pass
