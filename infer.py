6b069deae53019dc2708b960d077dc01a90c2e81
Jackson Huang <jackson_huang@live.com>
2017-12-20 17:59:06 +1100

    Inference now works with printed output

--- infer.py
+++ infer.py
@@ -1,11 +1,14 @@
 import argparse
 import os
+import json
 
 from model import ShuffleNet
 
 from torchvision import transforms
+from torch.autograd import Variable
 import torch
 from PIL import Image
+import numpy as np
 
 def get_transformer():
   normalize = transforms.Normalize(
@@ -13,40 +16,57 @@ def get_transformer():
       std=[0.229, 0.224, 0.225])
 
   transformer = transforms.Compose([
-      transforms.Resize(256),
-      transforms.CenterCrop(224),
+      transforms.Resize(128),
+      transforms.CenterCrop(128),
       transforms.ToTensor(),
       normalize
   ])
   return transformer
 
 def preprocess(image, transformer):
-  return transformer(image)
+  x = transformer(image)
+  return Variable(x.unsqueeze(0))
 
 def infer(args):
   # make ShuffleNet model
-  net = ShuffleNet(num_classes=1000, in_channels=3)
+  print('Creating ShuffleNet model')
+  net = ShuffleNet(num_classes=args.num_classes, in_channels=3)
   
   # load trained checkpoint
-  checkpoint = torch.load(args.checkpoint)
+  print('Loading checkpoint')
+  checkpoint = torch.load(args.checkpoint, map_location=lambda storage, loc: storage)
   net.load_state_dict(checkpoint['state_dict'])
+
+  print('Loading index-class map')
+  with open(args.idx_to_class, 'r') as f:
+      mapping = json.load(f)
 
   # image transformer
   transformer = get_transformer()
 
   # make input tensor
+  print('Loading image')
   image = Image.open(args.image)
+  print('Preprocessing')
   x = preprocess(image, transformer)
 
   # predict output
+  print('Inferring on image {}'.format(args.image))
   y = net(x)
-  print(y)
-  
+  top_idxs = np.argsort(y.data.cpu().numpy().ravel()).tolist()[-10:][::-1]
+  print('==========================================')
+  for i, idx in enumerate(top_idxs):
+    key = str(idx)
+    class_name = mapping[key][1]
+    print('{}.\t{}'.format(i+1, class_name))
+  print('==========================================')
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('image', help='Path to image that we want to classify')
-  parser.add_argument('checkpoint', help='Path to ShuffleNet checkpoint with trained weights')
-  parser.add_argument('idx_to_class', help='Path to JSON file mapping indexes to class names')
+  parser.add_argument('image', type=str, help='Path to image that we want to classify')
+  parser.add_argument('checkpoint', type=str, help='Path to ShuffleNet checkpoint with trained weights')
+  parser.add_argument('idx_to_class', type=str, help='Path to JSON file mapping indexes to class names')
+  parser.add_argument('--num_classes', type=int, help='Number of classes to predict', default=1000)
   args = parser.parse_args()
   infer(args)
